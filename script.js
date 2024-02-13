2d7128bcbd0ba2cd469f80ac4a402279587edd49
Rhet <meddakee2000@gmail.com>
2024-02-12 22:25:14 -0500

    ``prod-1.2``

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Fetch Manifest
     fetch('manifest.json').then(response => response.json()).then(data => {
-        versionLabel.innerHTML = `${data.build}-${data.version}`
+        versionLabel.innerHTML = `<a href="https://github.com/rhet0r1cal/CottenCandy-Archive">Visit on GitHub</a> - ${data.build}-${data.version}`
     })
 
 })
